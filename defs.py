f650ce634b252dd0893f43de87a4309307a59c2f
Joe B <joseph.bertino@gmail.com>
2023-05-20 21:58:07 -0400

    1) Moved some class and CONST defs to defs.py 2) Eliminated the need for reduce_player_possibles_from_hand by improving check_player_hand_size() 3) call check_player_hand_size if we find another clue in determine_clues

---
+++ defs.py
@@ -0,0 +1,71 @@
+import enum
+
+SUSPECT = enum.Enum('Suspect', ['white', 'plum', 'peacock', 'scarlet', 'mustard', 'green'])
+WEAPON = enum.Enum('Weapon', ['rope', 'pipe', 'wrench', 'candlestick', 'knife', 'revolver'])
+ROOM = enum.Enum('Room', ['billiard', 'lounge', 'conservatory', 'kitchen', 'hall',
+                          'dining', 'study', 'library', 'ballroom'])
+
+
+CATEGORIES = [SUSPECT, WEAPON, ROOM]
+ALL_CARDS = {value for category in CATEGORIES for value in category.__members__}
+NUM_CARDS = len(ALL_CARDS)
+
+
+class ClueCardSet:
+    def __set_name__(self, owner, name):
+        self.name = name + '_dict'
+
+    def __get__(self, instance, owner):
+        d = instance.__dict__[self.name]
+        return set([e for vs in d.values() for e in vs])
+
+    def __set__(self, instance, cards: set[str]):
+        instance.__dict__[self.name] = {}
+        for category in CATEGORIES:
+            clues = set(category.__members__) & cards
+            if clues:
+                instance.__dict__[self.name][category.__name__] = clues
+
+
+class Player(object):
+
+    hand = ClueCardSet()
+    possibles = ClueCardSet()
+
+    def __init__(self, number=0, size_hand=0, is_me=False, cards=None):
+        self.hand_dict = {}
+        self.possibles_dict = {}
+
+        self.is_me = is_me
+        if cards is None:
+            cards = []
+        self.size_hand = size_hand
+        self.number = number
+
+        if self.number == 0:
+            # Non-player should have empty sets
+            self.hand = set()
+            self.possibles = set()
+        else:
+            self.hand = set(cards if is_me else [])
+            self.possibles = set([] if is_me else ALL_CARDS - set(cards))
+
+
+class Turn(object):
+
+    suggestion = ClueCardSet()
+    possible_reveals = ClueCardSet()
+
+    def __init__(self, number: int = 0, suggestion=None, suggester: Player = None, revealer: Player = None):
+        self.suggestion_dict = {}
+        self.possible_reveals_dict = {}
+
+        if suggestion is None:
+            suggestion = set()
+        self.number = number
+        self.suggester: Player = suggester
+        self.revealer: Player = revealer
+        self.revealed_card = None
+        self.totally_processed = False
+        self.suggestion: set[str] = set(suggestion)
+        self.possible_reveals: set[str] = set(suggestion)
